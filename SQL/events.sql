af5aad5eea12ec0e67f7272d8ec548a403beb2ea
Hannes Filler <hfiller2@uwo.ca>
2017-03-06 17:22:05 -0500

    added events table

---
+++ SQL/events.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Events(
+	id serial primary key,
+	type varchar NOT NULL,
+	currency varchar NOT NULL,
+	category varchar NOT NULL,
+	startTime timestamp default NULL,
+	endTime timestamp default NULL,
+	eventId integer,
+	price float,
+	title text,
+	description text,	
+	longitude Double Precision,
+	latitude Double Precision,
+	api varchar
+);
