2695a4060d35af6038629916fcbb21a3f9066818
Ryan Holmes <ryankaz94@gmail.com>
2017-03-07 21:47:33 -0500

    getting tired

--- frontEnd/public/js/main.js
+++ frontEnd/public/js/main.js
@@ -1,4 +1,3 @@
-// user/event -> buttonCard, formCard, card
 $(document).ready(function(){
   var users;
   var numEvents;
@@ -24,31 +23,7 @@ function userButtonCardFn(){
   if(typeof(users) === "undefined"){ users = []; numEvents = 0;}
 
   $('#userButtonCard').remove();
-  var userForm = `
-  <div class="hidden col-2 offset-2" id="userFormCard">
-    <div class="card add-user-card">
-      <img src="../images/user_icon_${users.length + 1}.png" alt="User 1" class="circle-image">
-
-      <div class="col-10 offset-1">
-        <form action="">
-          <input class="input-field" type="text" placeholder="User ${users.length + 1}" id="userNameField">
-
-          <div class="select-style">
-            <select id="userLocationField">
-              <option value="Location 1">Location 1</option>
-              <option value="Location 2">Location 2</option>
-              <option value="Location 3">Location 3</option>
-              <option value="Location 4">Location 4</option>
-            </select>
-          </div>
-
-          <div class="button-pink" id="createUserBtn" style="width: 100%; margin-left: 2px; margin-top: 4px;">Create</div>
-        </form>
-      </div>
-
-    </div>
-  </div>
-  `
+  var userForm = getUserFormCard(users.length + 1);
   $(`#userRow${users.length}`).prepend(userForm);
   fadeInAnimate('userFormCard')
   $('#createUserBtn').on("click", createUserButton);
@@ -119,8 +94,148 @@ function eventButtonCardFn(){
   var eventId = `#${this.id}`;
   var row = eventId.split('').pop();
 
-  var eventCardForm = `
-  <div class="hidden col-5 remove-padding" id="eventFormCard${row}">
+  var eventCardForm = getEventFormCard(row);
+
+  $(eventId).remove();
+  $(`#eventContainer${row}`).append(eventCardForm);
+  fadeInAnimate(`eventFormCard${row}`);
+  $(`#eventSearchBtn${row}`).on("click", searchEvents);
+};
+
+// =======================================================================================================
+
+function createUserContinueBtn(){
+  //inject recommendation page
+  var recPage = `
+  <div class="recommend-events-background">
+    <h1 class='page-title'> Recommend Events </h1>
+
+    <div class="ryan-row" style='margin-top: 50px;'>
+      <div class="col-8 offset-2">
+        <div class="col-3">
+          <ul class="nav nav-tabs">
+            <li class="active tab-item"><a href="#u1" data-toggle="tab"> ${users[0].name} </a></li>`
+
+  for(var i = 1; i<=users.length-1; i++){
+    recPage = recPage.concat(`<li class="tab-item"><a href="#u${i+1}" data-toggle="tab"> ${users[i].name} </a></li>`);
+  }
+
+  recPage = recPage.concat(`
+        </ul>
+      </div>
+      <div class="tab-content col-9 events-container">`);
+
+  for(var i = 0; i<=users.length-1; i++){
+    recPage = recPage.concat(`<div class="tab-pane" id="u${i+1}">`);
+    for(var j=0; j<=users[i]["events"].length-1; j++){
+      var eCard = getEventCard(users[i]["events"][j]);
+      recPage = recPage.concat(eCard);
+    }
+    recPage = recPage.concat(`</div>`);
+  }
+
+  recPage = recPage.concat("</div></div></div></div></div>");
+
+  if(!$(`.recommend-events-background`).length){
+    $('body').append(recPage);
+    $('#u1').addClass("active fade in");
+    // $('#test').on("click", testing);
+  }
+
+  $('html, body').animate({
+     scrollTop: $('.recommend-events-background').offset().top
+   }, 1000);
+};
+
+// =======================================================================================================
+
+function createEventButton(){
+  var jq = $(`#${this.id}`);
+  var row = jq.data("row");
+  var eventData = { title: jq.text(), id: this.id, description: jq.data("description"), category: jq.data("category"), price: jq.data("price") }
+  var eventCard = getEventCard(eventData);
+
+  $(`#eventFormCard${row}`).remove();
+  $(`#eventContainer${row}`).append(eventCard);
+  $(`#eventCard${numEvents}`).addClass("hidden");
+  fadeInAnimate(`eventCard${numEvents}`);
+
+  var addEventCard = getEventButtonCard(row);
+  $(`#eventContainer${row}`).append(addEventCard);
+  fadeInAnimate(`eventButtonCard${row}`);
+
+  $(`#eventButtonCard${row}`).on("click", eventButtonCardFn);
+
+  //TODO: push full event not just title
+  users[row]["events"].push(eventData);
+  numEvents++;
+};
+
+// =======================================================================================================
+
+function searchEvents(){
+  var row = this.id.split("").pop();
+  var searchVal = $(`#event-search${row}`).val().toUpperCase();
+  $(`#eventSearchContainer${row}`).empty();
+
+  $.getJSON( "../data/VanLaNY3000output.json", function( data ) {
+    var items = [];
+    $.each( data, function(k,v) {
+      if(v.title.toUpperCase().includes(searchVal) || v.description.toUpperCase().includes(searchVal) || v.category.toUpperCase().includes(searchVal)){
+        items.push( `<div data-row="${row}" data-description="${v.description}" data-category="${v.category}"
+          data-price="${v.price}" class="event-listing" id="${v.id}"> ${v.title} </div>` );
+      }
+    });
+    $(`#eventSearchContainer${row}`).append(items.join(""));
+
+    $.each($(`.event-listing`), function(k, v){
+      $(`#${v.id}`).on("click", createEventButton);
+    });
+  });
+};
+
+// =======================================================================================================
+
+function fadeInAnimate(id){
+  $(`#${id}`).removeClass('hidden');
+  $(`#${id}`).addClass('animated fadeInDown');
+}
+
+function testing(){
+  console.log(users);
+};
+
+// ==============  GETTERS =======================
+function getEventCard(eventObject){
+  return (`
+  <div class="col-5 remove-padding" id="eventCard${numEvents}">
+    <div class="card event-card">
+      <div class="events-title"> ${eventObject.title} </div>
+      <div class="events-description"> ${eventObject.description} </div>
+      <div class="events-category"> ${eventObject.category} </div>
+      <div class="events-price"> $${eventObject.price} </div>
+    </div>
+  </div>
+  `);
+};
+
+// =======================================================================================================
+
+function getEventButtonCard(row){
+  return(`
+  <div class="hidden col-5 remove-padding"  id="eventButtonCard${row}">
+    <div class="card event-card card-hover">
+      <div class="add-text">
+        <span class="plus"> + </span> Add Event
+      </div>
+    </div>
+  <div>
+  `);
+};
+
+function getEventFormCard(row){
+  return (`
+    <div class="hidden col-5 remove-padding" id="eventFormCard${row}">
     <div class="card event-card">
 
       <div class="input-group event-search">
@@ -138,126 +253,33 @@ function eventButtonCardFn(){
 
     </div>
   </div>
-  `
+  `)
+}
 
-  $(eventId).remove();
-  $(`#eventContainer${row}`).append(eventCardForm);
-  fadeInAnimate(`eventFormCard${row}`);
-  $(`#eventSearchBtn${row}`).on("click", searchEvents);
-};
+function getUserFormCard(l){
+  return (`
+  <div class="hidden col-2 offset-2" id="userFormCard">
+    <div class="card add-user-card">
+      <img src="../images/user_icon_${l}.png" alt="User 1" class="circle-image">
 
-// =======================================================================================================
-
-function createUserContinueBtn(){
-  //inject recommendation page
-  var recPage = `
-  <div class="recommend-events-background">
-    <h1 class='page-title'> Recommend Events </h1>
-
-  <div class="ryan-row" style='margin-top: 50px;'>
       <div class="col-10 offset-1">
+        <form action="">
+          <input class="input-field" type="text" placeholder="User ${l}" id="userNameField">
 
-        <div class="col-2">
-          <ul class="nav nav-tabs">
-            <li class="active tab-item"><a href="#u1" data-toggle="tab"> ${users[0].name} </a></li>`
+          <div class="select-style">
+            <select id="userLocationField">
+              <option value="Location 1">Location 1</option>
+              <option value="Location 2">Location 2</option>
+              <option value="Location 3">Location 3</option>
+              <option value="Location 4">Location 4</option>
+            </select>
+          </div>
 
-  for(var i = 1; i<=users.length-1; i++){
-    recPage = recPage.concat(`<li class="tab-item"><a href="#u${i+1}" data-toggle="tab"> ${users[i].name} </a></li>`);
-  }
+          <div class="button-pink" id="createUserBtn" style="width: 100%; margin-left: 2px; margin-top: 4px;">Create</div>
+        </form>
+      </div>
 
-  recPage = recPage.concat(`
-          </ul>
-        </div>
-        <div class="col-10">
-          <div class="tab-content">
-            <div class="tab-pane active fade in" id="u1">
-               poop1
-               <!-- <div class="button-pink pull-right"> Recommend </div> -->
-            </div>`);
-
-  for(var i = 1; i<=users.length-1; i++){
-    recPage = recPage.concat(`
-      <div class="tab-pane" id="u${i+1}">
-        <!-- Events go here -->
-        <div class="button-pink pull-right"> Recommend </div>
-      </div>`);
-  }
-
-  recPage = recPage.concat("</div></div></div></div></div>");
-
-  if(!$(`.recommend-events-background`).length){
-    $('body').append(recPage);
-  }
-
-  $('html, body').animate({
-     scrollTop: $('.recommend-events-background').offset().top
-   }, 1000);
-};
-
-// =======================================================================================================
-
-function createEventButton(){
-  var jq = $(`#${this.id}`);
-  var row = jq.data("row");
-
-  var eventCard = `
-  <div class="hidden col-5 remove-padding" id="eventCard${numEvents}${row}">
-    <div class="card event-card">
-      <div class="events-title"> ${jq.text()} </div>
     </div>
   </div>
-  `
-
-  $(`#eventFormCard${row}`).remove();
-  $(`#eventContainer${row}`).append(eventCard);
-  fadeInAnimate(`eventCard${numEvents}${row}`);
-
-  var addEventCard = getEventButtonCard(row);
-  $(`#eventContainer${row}`).append(addEventCard);
-  fadeInAnimate(`eventButtonCard${row}`);
-
-  $(`#eventButtonCard${row}`).on("click", eventButtonCardFn);
-  //TODO: push full event not just title
-  users[row]["events"].push(jq.text());
-  numEvents++;
+  `);
 };
-
-// =======================================================================================================
-
-function getEventButtonCard(row){
-  var addEventCard = `
-  <div class="hidden col-5 remove-padding"  id="eventButtonCard${row}">
-    <div class="card event-card card-hover">
-      <div class="add-text">
-        <span class="plus"> + </span> Add Event
-      </div>
-    </div>
-  <div>
-  `
-  return addEventCard;
-};
-
-function searchEvents(){
-  var row = this.id.split("").pop();
-  var searchVal = $(`#event-search${row}`).val().toUpperCase();
-  $(`#eventSearchContainer${row}`).empty();
-
-  $.getJSON( "../data/VanLaNY3000output.json", function( data ) {
-    var items = [];
-    $.each( data, function(k,v) {
-      if(v.title.toUpperCase().includes(searchVal) || v.description.toUpperCase().includes(searchVal) || v.category.toUpperCase().includes(searchVal)){
-        items.push( `<div data-row="${row}" class="event-listing" id="${v.id}"> ${v.title} </div>` );
-      }
-    });
-    $(`#eventSearchContainer${row}`).append(items.join(""));
-
-    $.each($(`.event-listing`), function(k, v){
-      $(`#${v.id}`).on("click", createEventButton);
-    });
-  });
-};
-
-function fadeInAnimate(id){
-  $(`#${id}`).removeClass('hidden');
-  $(`#${id}`).addClass('animated fadeInDown');
-}
