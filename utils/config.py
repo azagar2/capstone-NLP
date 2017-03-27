3b70e310f91db8690f847cb3a0686b72b074b239
Hannes Filler <hfiller2@uwo.ca>
2017-03-27 18:09:35 -0400

    added caching options, also modularized config

---
+++ utils/config.py
@@ -0,0 +1,40 @@
+import psycopg2;
+import json;
+import os;
+
+class Config:
+	class __Config:
+		DEBUG = False;
+		data = {};
+		# Singleton used to access the database
+		# @param {String} connection configuration text
+		def __init__(self,config):
+			self.data = config;
+
+	# instance of singleton
+	instance = None
+
+	# singleton constructor
+	def __init__(self):
+		if not Config.instance:
+			fileDir = os.path.dirname(os.path.realpath('__file__'));
+			fileName = os.path.join(fileDir, 'frontEnd','config','config.live.json');
+			try:
+				with open(fileName) as data_file:
+					config_data = json.load(data_file);
+			except IOError:
+				fileName = os.path.join(fileDir, 'frontEnd','config','config.dev.json');
+				try:
+					with open(fileName) as data_file:
+						config_data = json.load(data_file);
+				except IOError:
+					print("no config file found");
+					sys.exit();
+			except json.JSONDecodeError:
+				print('Invalid output content')
+				sys.exit()
+			Config.instance = Config.__Config(config_data);
+
+	# proxy function
+	def __getattr__(self, name):
+		return getattr(self.instance, name);
